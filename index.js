4a773f46f47e2a884f8506da5e1bfba89be4e333
Tomasz Dańczak <tomaszdanczak@leancode.pl>
2022-05-23 21:34:49 +0200

    Use PORT env variable

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
+const PORT = process.env.PORT || 4000;
 
 const typeDefs = gql`
   type Query {
@@ -23,4 +24,4 @@ const data = {
 
 const server = new ApolloServer({ typeDefs, rootValue: data });
 
-server.listen({ port: 4000 }).then((result) => console.log(result.url));
+server.listen({ port: PORT }).then((result) => console.log(result.url));
